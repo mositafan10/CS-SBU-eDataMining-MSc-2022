52d64a4eeecf26990d6d6e127170f737c4a1cd0a
mosi <mosi@mosi.com>
2022-07-01 11:05:41 +0430

    first commit

---
+++ service3/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Service3Config(AppConfig):
+    name = 'service3'
---
+++ service4/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Service4Config(AppConfig):
+    name = 'service4'
